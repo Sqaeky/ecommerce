c81b33e62805402ffd86f78763f2a05ef5ca9781
Baladee <honzikhalatka@post.cz>
2026-08-24 20:08:02 +0200

    Fixed 403 Forbidden on error

--- src/main/kotlin/cz/baladee/ecommerce/user/config/SecurityConfig.kt
+++ src/main/kotlin/cz/baladee/ecommerce/user/config/SecurityConfig.kt
@@ -28,7 +28,7 @@ class SecurityConfig(
             .csrf { it.disable() }
             .authorizeHttpRequests { auth ->
                 auth
-                    .requestMatchers("/auth/**").permitAll() // login + register
+                    .requestMatchers("/auth/**", "/error").permitAll() // login + register
                     .anyRequest().authenticated()
             }
             .sessionManagement { session ->
